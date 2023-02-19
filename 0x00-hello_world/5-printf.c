8ba8a12a5350d3ec82de75c3ec5b8b7019909f4a
ogetatonny <tonnyogeta7598@gmail.com>
2023-02-19 02:40:16 -0800

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,12 @@
 
 /**
  * main-5-printf.c
- * Retun: 0 when successful
+ *
+ * Retun: 0 on success, non-zero on failure
  */
 
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)");
+	printf("Size of an int: 4 byte(s)");
+	printf("Size of a long int: 4 byte(s)");
+	printf("Size of a long long int: 8 byte(s)");
+	printf("Size of a float: 4 byte(s)");
+	return (0);
+
+}
