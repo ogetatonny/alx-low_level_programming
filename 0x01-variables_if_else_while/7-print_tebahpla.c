8fead57d5f0f66b9487fc1a5ae50b6eaa890f24b
ogetatonny <tonnyogeta7598@gmail.com>
2023-02-20 02:40:50 -0800

    lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase alphabet in reverse
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	char m;
+
+	for (m = 'z'; m <= 'a'; m++)
+		putchar (m);
+	putchar ('\n');
+	return (0);
+
+}
