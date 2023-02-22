6a1c1a794de7e46c306a40a05700827b9b0f4202
ogetatonny <tonnyogeta7598@gmail.com>
2023-02-22 10:55:50 -0800

    function that prints sum of integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - fuction that two integers and returns result
+ *
+ * @c: function parameter
+ * @d: funtion parameter
+ * Return: (c + d) when successful
+ */
+
+int add(int c, int d)
+{
+	return (c + d);
+}
