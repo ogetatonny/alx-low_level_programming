3494b1c7b7e408ceda83eb2e470c1811cbae4f3e
ogetatonny <tonnyogeta7598@gmai.com>
2023-04-12 14:31:05 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper -function of uppercase letters
+ * @c: checks character c
+ * Return: 0 if successful
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (0);
+}
+
